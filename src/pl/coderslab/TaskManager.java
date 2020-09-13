ffe76b9fdb126d4d2a7c7bab9bd026ee121af32d
e-sprit56 <ksamburski@gmail.com>
2020-09-13 14:08:54 +0200

    taskmamange

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,40 +1,181 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.GenericValidator;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class TaskManager {
 
-    public static void menu(){
-        System.out.println(ConsoleColors.BLUE + "Pleas select an option");
-        System.out.println(ConsoleColors.RESET +"");
-        System.out.println("list");
-        System.out.println("add");
-        System.out.println("remove");
-        System.out.println("exit");
+    public static String FILE = "tasks.csv";
+
+
+    public static void menu() {
+
+        System.out.println("\n \n \n");
+        System.out.println("!!!!  Welcome in Task Manager 2020 XP PRO   !!!!");
+        System.out.println("**************************************************");
+        System.out.println(" ");
+        boolean running = true;
+
+        do {
+
+            System.out.println(ConsoleColors.BLUE + "Pleas select an option");
+            System.out.println(ConsoleColors.RESET + "");
+            System.out.println("list");
+            System.out.println(ConsoleColors.GREEN_BOLD +"add" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.RED_BOLD + "remove" + ConsoleColors.RESET);
+            System.out.println("exit");
+            System.out.println(ConsoleColors.RESET);
+
+            Scanner scanner = new Scanner(System.in);
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "list" -> read();
+                case "add" -> add();
+                case "remove" -> remove();
+                case "exit" -> running=false;
+                default -> System.out.println("Invalid command");
+            }
+        }while (running);
+
+        System.out.println();
     }
 
-    public static void read(){
 
-        Path path = Paths.get("tasks.csv");
+    public static void read() {
 
-        try{
-            for(String line: Files.readAllLines(path)) {
-                System.out.println(line);
+        String[] lines = dataImport();
+
+        System.out.println("Your current task list: \n");
+
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(i+1 + " || " + lines[i]);
+        }
+    }
+        public static void add (){
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Pleas enter a new task:");
+            System.out.println("Task name");
+            String task = scanner.nextLine();
+
+
+
+            String date;
+            do{
+                System.out.println("Due Date in format: yyyy-mm-dd");
+                date = scanner.nextLine();
+
+                if(GenericValidator.isDate(date, "yyyy-mm-dd", true)){
+                    break;
+                }else System.out.println("Invalid date format");
+            }while (true);
+
+
+            String important;
+            do {
+                System.out.println("Important: True or False");
+                important = scanner.nextLine().toLowerCase();
+
+                if(important.equals("true")||important.equals("false")){
+                    break;
+                }else System.out.println("please enter True or False");
+
+
+            }while(true);
+
+            String newEntry = String.join(", " ,task, date, important );
+
+            try {
+                FileWriter writer = new FileWriter(FILE, true);
+                writer.append(newEntry); // usuniety powrót karetki
+                writer.close();
+            }catch (IOException e){
+                System.out.println("File not found");
             }
-        }catch (IOException e){
-            System.err.println("Brak Pliku");
+        }
 
+        public static void remove(){
+
+            String[] lines = dataImport();
+            Scanner scan = new Scanner(System.in);
+
+
+            read();
+            System.out.println("");
+
+            int taskNumber;
+            while(true){
+                System.out.println("Which task number do you want to delete?");
+                try {
+                    taskNumber = scan.nextInt();
+                }catch (NumberFormatException e){
+                    System.out.println("Invalid format");
+                    continue;
+                }
+                break;
+            }
+
+            lines= ArrayUtils.remove(lines,taskNumber-1);
+            try {
+                FileWriter writer = new FileWriter(FILE, false);
+                for (int j=0; j<lines.length; j++) {
+                    String s = lines[j];
+                    writer.append(s + "\n");
+                }
+                writer.close();
+            }catch (IOException e){
+                System.out.println("File not found");
+            }
+            System.out.println("Your updated task list:");
+            read();
         }
 
 
+    public static String [] dataImport () {
+
+
+        Path path = Paths.get(FILE);
+        List<String> lines = new ArrayList<String>();
+
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            System.err.println("Brak Pliku");
+        }
+
+        String[] tab = new String[lines.size()];
+        for (int i = 0; i < lines.size(); i++) {
+            tab[i] = lines.get(i);
+        }
+        return tab;
     }
+
+
 
     public static void main(String[] args) {
 
-        read();
+        menu();
+
+
+
+
+
+
 
 
 
@@ -45,3 +186,14 @@ public class TaskManager {
 
     }
 }
+
+
+//        Path path = Paths.get(FILE);
+
+//        try{
+//            if(!Files.exists(path)){
+//                Files.createFile(path);
+//            }
+//        }catch (IOException ignored){}
+
+//System.out.println((dataImport()));
